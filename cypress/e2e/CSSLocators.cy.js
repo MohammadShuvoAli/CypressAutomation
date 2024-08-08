b0578648f2acf54b29689b7fb923d9cd0360a460
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-09 00:31:50 +0600

    updated

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -2,7 +2,7 @@ describe('CSSLocator', () =>{
     it('csslocator', () => {
         cy.visit('https://tutorialsninja.com/demo/index.php')
         cy.get("input[placeholder='Search']").type("iphone") // id tag is optional
-        cy.get(".fa.fa-search']").click()
+        cy.get("button[class='btn btn-default btn-lg']").click()
         cy.get("div[id='content'] h1").contains("Search - iphone") // Assertion
     })
 })
--- cypress/e2e/XPathLocators.cy.js
+++ cypress/e2e/XPathLocators.cy.js
@@ -2,7 +2,7 @@ require('cypress-xpath');
 
 describe('XPathLocators', () => {
     it('find no of products', () => {
-        cy.visit("https://demo.nopcommerce.com")
-        cy.xpath("//*[@data-productid]").should('have.length', 4)
+        cy.visit("https://tutorialsninja.com/demo/index.php")
+        cy.xpath('//*[@class="product-thumb transition"]').should('have.length', 4)
     })
 })
