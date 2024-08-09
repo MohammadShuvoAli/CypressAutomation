7ed13b941aa80707a3c533b20761dc7220c036aa
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-09 23:08:55 +0600

    and assertions

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -2,10 +2,20 @@ describe("Assertions", () => {
     it("Implicit Assertions", () => {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php")
         
-        // should
+        // should assertions
+        // cy.url().should('include', 'orangehrmlive.com')
+        // cy.url().should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        // cy.url().should('contain', 'orangehrm')
+
+        /*cy.url().should('include', 'orangehrmlive.com')
+        .should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        .should('contain', 'orangehrm')*/
+
+        // and assertions
         cy.url().should('include', 'orangehrmlive.com')
-        cy.url().should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.url().should('contain', 'orangehrm')
+        .and('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        .and('contain', 'orangehrm')
+        .and('not.contain', 'redhrm')
 
     })
 })
