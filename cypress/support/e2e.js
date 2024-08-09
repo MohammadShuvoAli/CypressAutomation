34980117011ac30b763c51440438a84a8020d8a5
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-09 22:54:36 +0600

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 require('cypress-xpath');
-require("cypress");
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
