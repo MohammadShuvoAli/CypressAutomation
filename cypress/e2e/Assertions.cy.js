7ebe5b6b709aa2165b35b9727c689321406d2e06
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-09 22:54:41 +0600

    Update Assertions.cy.js

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -2,6 +2,10 @@ describe("Assertions", () => {
     it("Implicit Assertions", () => {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php")
         
+        // should
+        cy.url().should('include', 'orangehrmlive.com')
+        cy.url().should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.url().should('contain', 'orangehrm')
 
     })
 })
