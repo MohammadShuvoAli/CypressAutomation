b272d5458ef8659996c27865646310048de2a85c
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-10 16:22:58 +0600

    update

--- cypress/e2e/RadioButton.cy.js
+++ cypress/e2e/RadioButton.cy.js
@@ -2,9 +2,19 @@ describe("Radio Button", ()=> {
     it("Handle Radio Button", () => {
         cy.visit("https://testautomationpractice.blogspot.com/")
 
+        // Visibility of Radio Button
         cy.get("label[for='gender']").should("be.visible")
-        cy.get("label[for='male']").should("be.visible")
-        cy.get("label[for='female']").should("be.visible")
+        cy.get("input#male").should("be.visible")
+        cy.get("input#female").should("be.visible")
+
+        // Selecting Radio Button
+        cy.get("input#male").check().should("be.checked")
+        cy.get("input#female").should("not.be.checked")
+
+        cy.wait(1000) // wait for 1 sec
+
+        cy.get("input#female").check().should("be.checked")
+        cy.get("input#male").should("not.be.checked")
 
     })
 
