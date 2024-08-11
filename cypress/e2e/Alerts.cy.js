b9c142886d0ea3618585dfef9e61a35a8a0dc298
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-11 22:57:16 +0600

    javascript confirm alert

--- cypress/e2e/Alerts.cy.js
+++ cypress/e2e/Alerts.cy.js
@@ -1,6 +1,6 @@
 describe("Alerts", () => {
     // Javascript Alert: It wil have some text and 'OK' button
-    it("javascript alert", () => {
+    it.skip("javascript alert", () => {
         cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
         cy.get("button[onclick='jsAlert()']").click()
         // js alert validation
@@ -8,7 +8,20 @@ describe("Alerts", () => {
             expect(message).to.contains("I am a JS Alert")
         })
 
-        // alert window automatically closed by cypress
+        // alert window automatically closed (Pressing OK) by cypress
         cy.get("#result").should("have.text", "You successfully clicked an alert")
+    })
+
+    // Javascript Confirm Alert: It will have some text with 'OK' and 'Cancel' buttons
+    it("javascript confirm alert", () => {
+        cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
+        cy.get("button[onclick='jsConfirm()']").click()
+        // js alert validation
+        cy.on("window:confirm",(message) => {
+            expect(message).to.contains("I am a JS Confirm")
+        })
+
+        // alert window automatically closed (Pressing Confirm) by cypress
+        cy.get("#result").should("have.text", "You clicked: Ok")
     })
 })
