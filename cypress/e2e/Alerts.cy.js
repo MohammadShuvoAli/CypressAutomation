a4cbb27d72bfadffceeb263b84a8a850d4e1cc01
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-13 00:43:14 +0600

    Update Alerts.cy.js

--- cypress/e2e/Alerts.cy.js
+++ cypress/e2e/Alerts.cy.js
@@ -79,7 +79,7 @@ describe("Alerts", () => {
     })
 
     it.only("authenticated alert - method 2", () => {
-        cy.visit("https://the-internet.herokuapp.com/basic_auth", {auth: {username: "admin", password:"admin"}})
+        cy.visit("https://admin:admin@the-internet.herokuapp.com/basic_auth")
 
         cy.get("div[class='example'] p").should("have.contain", "Congratulations!")
     })
