1d75fb454e0d19e55708644f21bf1632d6315f22
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 00:05:46 +0600

    Update Table.cy.js

--- cypress/e2e/Table.cy.js
+++ cypress/e2e/Table.cy.js
@@ -6,6 +6,13 @@ describe('Handle Tables', () => {
         cy.get("table#productTable>tbody>tr").should("have.length", "5");
         // Table columns
         cy.get("table#productTable>thead>tr>th").should("have.length", "4");
+    })
 
+    it('check cell data from specific row & column', () => {
+        cy.visit("https://testautomationpractice.blogspot.com/")
+        // specific cell data
+        cy.get("table#productTable>tbody>tr:nth-child(4)>td:nth-child(1)").contains("4")
+        cy.get("table#productTable>tbody>tr:nth-child(4)>td:nth-child(2)").contains("Product 4")
+        cy.get("table#productTable>tbody>tr:nth-child(4)>td:nth-child(3)").contains("$7.99")
     })
 })
