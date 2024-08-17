2c564c11b112fb62ce5e1736c0f066f5e15a1f3c
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 22:47:32 +0600

    update

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here if needed
     },
+    video: true, // Enable video recording
+    screenshotsFolder: "cypress/screenshots", // Folder where screenshots will be saved
+    videosFolder: "cypress/videos", // Folder where videos will be saved
+    screenshotOnRunFailure: true, // Capture screenshots on test failure
+    videoCompression: 0, // Set video compression (0 for no compression, higher numbers for more compression)
   },
 });
