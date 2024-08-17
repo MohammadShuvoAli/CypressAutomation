e6dca146a4f9d21a08426253d1b80ee5e862de33
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 15:29:24 +0600

    update

--- cypress/e2e/FileUpload.cy.js
+++ cypress/e2e/FileUpload.cy.js
@@ -6,5 +6,7 @@ describe("File Uploads", () => {
         cy.get("#file-upload").should("be.visible").attachFile("test1.pdf")
         // click on upload button
         cy.get("#file-submit").should("be.visible").click()
+        // file upload validation
+        cy.get('h3').should('be.visible').and('contain', 'File Uploaded!')
     })
 })
