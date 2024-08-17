451cc5eab2a9b826ef77cb93c127e85f8931b5f2
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 20:36:55 +0600

    update

--- cypress/e2e/DataDrivenTest.cy.js
+++ cypress/e2e/DataDrivenTest.cy.js
@@ -1,10 +1,24 @@
 describe("Data Driven Test", () => {
     it("data driven test", () => {
         cy.fixture("orangehrmLogin").then((data) => {
-            data.foreach((userdata) => {
+
+            cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+            data.forEach((userdata) => {
                 cy.get("input[placeholder='Username']").type(userdata.username)
                 cy.get("input[placeholder='Password']").type(userdata.password)
                 cy.get("button[type='submit']").click()
+
+                if(userdata.username == "Admin" && userdata.password == "admin123"){
+                    cy.get(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module").contains(userdata.expected) 
+                    cy.get('.oxd-userdropdown-tab').click()
+                    cy.wait(1000); // Waits for 1 seconds
+                    cy.get(':nth-child(4) > .oxd-userdropdown-link').click()
+                }
+                else{
+                    cy.get(".oxd-text.oxd-text--p.oxd-alert-content-text").contains(userdata.expected)
+                }
+                
             })
         })
     })
