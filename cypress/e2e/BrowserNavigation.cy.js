35f9b8e85adeb40b2acdd0e2cb6a13df04697eaf
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 22:07:33 +0600

    update

--- cypress/e2e/BrowserNavigation.cy.js
+++ cypress/e2e/BrowserNavigation.cy.js
@@ -1,13 +1,14 @@
 describe("Browser Navigation", () => {
     it("Navigation Test", () => {
         cy.visit("https://demo.opencart.com/")
+        cy.wait(5000)
         cy.title().should("eq", "Your Store");
 
-        cy.get(".nav-link.dropdown-toggle.show").trigger("mouseover").click()
+        cy.get(".nav > :nth-child(1) > .dropdown-toggle").trigger("mouseover").click()
         cy.get("#narbar-menu > ul > li:nth-child(1) > div > div > ul > li:nth-child(2) > a").click()
         cy.get("div[id='content'] h2").contains("Desktops")
 
-        cy.go("back") //
+        cy.go("back") // home
         cy.title().should("eq", "Your Store");
 
         cy.go("forward") // desktop
