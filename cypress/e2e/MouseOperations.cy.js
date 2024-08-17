7c024ee28a10329c84be1c358c8c72f496a12a46
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 13:28:13 +0600

    Update MouseOperations.cy.js

--- cypress/e2e/MouseOperations.cy.js
+++ cypress/e2e/MouseOperations.cy.js
@@ -1,6 +1,13 @@
 describe("Mouse Operations", () => {
     it("Mouse Hover", () => {
         cy.visit("https://demo.opencart.com/")
-        cy.get("div#narbar-menu>ul>li:nth-child(3)").trigger("mouseover")
+        // drop down menu option should not be visible
+        cy.get("#narbar-menu > ul > li:nth-child(3) > div > div > ul > li:nth-child(2) > a").should("not.be.visible")
+        // hover over menu
+        cy.get("div#narbar-menu>ul>li:nth-child(3)").trigger("mouseover").click()
+        // option should be present
+        cy.get("#narbar-menu > ul > li:nth-child(3) > div > div > ul > li:nth-child(2) > a").should("be.visible")
     })
+
+    
 })
