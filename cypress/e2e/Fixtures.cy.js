f597832cea3ea1c5f2ea9af2d11eca9bd4a68851
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 20:20:52 +0600

    update

--- cypress/e2e/Fixtures.cy.js
+++ cypress/e2e/Fixtures.cy.js
@@ -10,5 +10,20 @@ describe("Fixtures", () => {
         })     
     })
 
-    
+    let userdata;
+    before(() => {
+        cy.fixture("orangehrm").then((data) => {
+            userdata = data
+        })
+    })
+
+    // for multiple it blocks
+    it.only("access through hook", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+        cy.get("input[placeholder='Username']").type(userdata.username)
+        cy.get("input[placeholder='Password']").type(userdata.password)
+        cy.get("button[type='submit']").click()
+        cy.get(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module").contains(userdata.expected) 
+    })
 })
