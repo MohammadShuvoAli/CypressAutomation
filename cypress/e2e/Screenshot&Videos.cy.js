b8d1dd674c22c868c5bd79206dfa31c77b900fe0
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 22:42:55 +0600

    update

--- cypress/e2e/Screenshot&Videos.cy.js
+++
@@ -1,8 +0,0 @@
-describe("Capture Screenshot and videos", ()=>{
-    it.skip("Capture Screenshot",()=>{
-        cy.visit("https://demo.opencart.com/");
-        cy.wait(5000);
-        cy.screenshot("Homepage");
-        cy.get("#logo > a > .img-fluid").screenshot("Logo");
-    });
-});
---
+++ cypress/e2e/ScreenshotAndVideos.cy.js
@@ -0,0 +1,17 @@
+describe("Capture Screenshot and videos", ()=>{
+    it.skip("Capture Screenshot",()=>{
+        cy.visit("https://demo.opencart.com/");
+        cy.wait(5000);
+        cy.screenshot("Homepage");
+        cy.get("#logo > a > .img-fluid").screenshot("Logo");
+    });
+
+    it("Automatically Capture Screenshot & Video on Test Fail",()=>{
+        cy.visit("https://demo.opencart.com/");
+        cy.wait(5000);
+        
+        cy.get(".nav > :nth-child(1) > .dropdown-toggle").trigger("mouseover").click()
+        cy.get("#narbar-menu > ul > li:nth-child(1) > div > div > ul > li:nth-child(2) > a").click()
+        cy.get("div[id='content'] h2").contains("Software")
+    });
+});
