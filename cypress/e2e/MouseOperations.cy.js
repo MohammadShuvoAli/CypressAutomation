ab8922bea967284ec5ee8359913cd7d104c22b57
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 13:51:28 +0600

    update

--- cypress/e2e/MouseOperations.cy.js
+++ cypress/e2e/MouseOperations.cy.js
@@ -22,4 +22,16 @@ describe("Mouse Operations", () => {
         cy.get('.context-menu-icon-edit > span').should("be.visible")
         cy.get(".context-menu-icon-quit").click()
     })
+
+    it("Double Click", () => {
+        cy.visit("https://testautomationpractice.blogspot.com/")
+        // check heading
+        cy.get("div[id='HTML10'] h2[class='title']").should("be.visible").contains("Double Click")
+        // check first field value
+        cy.get('#field1').should("be.visible").should("have.value", "Hello World!")
+        // click on button
+        cy.get("button[ondblclick='myFunction1()']").trigger("dblclick").click()
+        // check if values has been copied to field 2
+        cy.get('#field2').should("be.visible").should("have.value", "Hello World!")
+    })
 })
