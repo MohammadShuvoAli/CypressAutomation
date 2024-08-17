877ac8abd0695fe6f0c506e37d10713f45ddfb6b
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 15:46:33 +0600

    update

--- cypress/e2e/FileUpload.cy.js
+++ cypress/e2e/FileUpload.cy.js
@@ -10,7 +10,7 @@ describe("File Uploads", () => {
         cy.get('h3').should('be.visible').and('contain', 'File Uploaded!')
     })
 
-    it.only("file upload - rename", () => {
+    it("file upload - rename", () => {
         cy.visit("https://the-internet.herokuapp.com/upload")
         // rename file before uploading
         cy.get("#file-upload").should("be.visible").attachFile({filePath:'test2.pdf', fileName:'TestPDFFileCypress.pdf'})
@@ -19,5 +19,13 @@ describe("File Uploads", () => {
         // file upload validation
         cy.get('h3').should('be.visible').and('contain', 'File Uploaded!')
         cy.get('#uploaded-files').should("contain","TestPDFFileCypress.pdf")
+    })
+
+    it("file upload - drag and drop", () => {
+        cy.visit("https://the-internet.herokuapp.com/upload")
+        // upload file using drag and drop
+        cy.get("#drag-drop-upload").attachFile("test1.pdf", {subjectType:'drag-n-drop'})
+        // validate dropped file
+        cy.get('#drag-drop-upload > .dz-preview > .dz-details > .dz-filename > span').should("be.visible").and("contain", "test1.pdf")
     })
 })
