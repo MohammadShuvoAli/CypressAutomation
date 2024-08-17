a5585b1176bcf49f84d5a47a68859a5d42b1cac5
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 15:23:09 +0600

    update

---
+++ cypress/e2e/FileUpload.cy.js
@@ -0,0 +1,10 @@
+describe("File Uploads", () => {
+    it("single file upload", () => {
+        cy.visit("https://the-internet.herokuapp.com/upload")
+        
+        // upload file
+        cy.get("#file-upload").should("be.visible").attachFile("test1.pdf")
+        // click on upload button
+        cy.get("#file-submit").should("be.visible").click()
+    })
+})
