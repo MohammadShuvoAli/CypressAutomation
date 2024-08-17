9510eeea1fef7b70514ecbb5b6c6d20bc6f00245
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-17 20:53:42 +0600

    update

--- cypress/e2e/CustomCommands.cy.js
+++ cypress/e2e/CustomCommands.cy.js
@@ -1,6 +1,7 @@
 describe("Custom Commands", () => {
     it("linktext custom command", () => {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.wait(5000)
         cy.linkText("OrangeHRM, Inc").click()
     })
 })
