b90167dc40b69aa22851e47f21c731072b32b842
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-18 15:32:20 +0600

    update

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -42,8 +42,8 @@ describe("Assertions", () => {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php")
 
         // fill form
-        cy.get("input[placeholder='username']").type("Admin")
-        cy.get("input[placeholder='password']").type("admin123")
+        cy.get("input[placeholder='Username']").type("Admin")
+        cy.get("input[placeholder='Password']").type("admin123")
         // login
         cy.get("button[type='submit']").click()
 
