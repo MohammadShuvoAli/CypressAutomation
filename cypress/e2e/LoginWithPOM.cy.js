6e158378e26f92ffb14c27482865d3615f75240f
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-18 15:32:29 +0600

    update

--- cypress/e2e/LoginWithPOM.cy.js
+++ cypress/e2e/LoginWithPOM.cy.js
@@ -1,5 +1,11 @@
 describe("POM", ()=>{
     it("login test with POM", ()=>{
-        
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php")
+
+        // fill form
+        cy.get("input[placeholder='Username']").type("Admin")
+        cy.get("input[placeholder='Password']").type("admin123")
+        // login
+        cy.get("button[type='submit']").click()
     })
 })
