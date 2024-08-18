e755ad050d4fdb94d0d4eb16a8f47cae4315f320
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-18 15:44:29 +0600

    update

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -27,8 +27,8 @@ describe("Assertions", () => {
         cy.xpath("//a").should('have.length', '5') // no of links
 
         // fill form
-        cy.get("input[placeholder='username']").type("Admin")
-        cy.get("input[placeholder='password']").type("admin123")
+        cy.get("input[placeholder='Username']").type("Admin")
+        cy.get("input[placeholder='Password']").type("admin123")
         // login
         cy.get("button[type='submit']").click()
 
