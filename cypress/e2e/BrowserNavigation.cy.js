f0250ee27beae80b676ba5cc9c3bb11013985380
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-19 23:16:15 +0600

    update

--- cypress/e2e/BrowserNavigation.cy.js
+++ cypress/e2e/BrowserNavigation.cy.js
@@ -1,16 +1,15 @@
 describe("Browser Navigation", () => {
     it("Navigation Test", () => {
-        cy.visit("https://www.globalsqa.com/demo-site/")
-        cy.wait(5000)
-        cy.title().should("eq", "Testing Demo Site | Practice Website For Automation - Global SQA");
+        cy.visit("https://magento.softwaretestingboard.com/")
+        cy.title().should("eq", "Home Page");
 
-        cy.get("li[id='menu-item-6898'] a").click()
-        cy.get("div[class='page_heading'] h1").contains("List Of CheatSheets")
+        cy.get("a[id='ui-id-3'] span:nth-child(1)").click()
+        cy.get(".base").contains("What's New")
 
         cy.go("back") // home
-        cy.title().should("eq", "Testing Demo Site | Practice Website For Automation - Global SQA");
+        cy.title().should("eq", "Home Page");
 
         cy.go("forward") // cheat page
-        cy.get("div[class='page_heading'] h1").contains("List Of CheatSheets")
+        cy.get(".base").contains("What's New")
     })
 })
